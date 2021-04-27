2dde1c0331dfebc26c9f38d3579a877ef7d670f8
Miguel Arreaza <mg.arreaza.13@gmail.com>
2021-04-27 11:05:05 -0400

    feat: add storage for user and login to context api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./components/login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./components/player";
+import { useDataLayerValue } from "./StateProvider";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hola Mundo</h1>
-    </div>
-  );
+
+  const [{ user, token }, dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if (_token) {
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token
+      })
+
+      spotify.setAccessToken(_token);
+      spotify
+        .getMe()
+        .then((user) => {
+          dispatch({
+            type: "SET_USER",
+            user: user,
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, []);
+
+  return <div className="app">{token ? <Player spotify={spotify} /> : <Login />}</div>;
 }
 
 export default App;
