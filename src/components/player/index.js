3266da3c547becf9db3bc3460f19a0761063605e
Miguel Arreaza <mg.arreaza.13@gmail.com>
2021-04-27 11:05:21 -0400

    feat: init player component

---
+++ src/components/player/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { PlayerContainer } from './styles'
+function Player({ spotify }) {
+  return (
+    <PlayerContainer>
+      <h1>Player</h1>
+    </PlayerContainer>
+  )
+}
+
+export default Player
